31790a0ffe1a63fd98945e8f84e0f9881d1c8741
Evgeniy Matveev <mfb.eugene@gmail.com>
2019-08-20 23:22:30 +0300

    Add OpenH264

--- setup.sh
+++ setup.sh
@@ -99,6 +99,11 @@ gvfs-smb `#gnome<>samba` \
 dnf-plugins-core `#Provides the commands to manage your DNF repositories from the command line.` \
 xorg-x11-drv-amdgpu `#AMDGPU driver for X11` \
 
+# Install OpenH264 from Cisco
+sudo dnf config-manager --set-enabled fedora-cisco-openh264
+sudo dnf install -y \
+gstreamer1-plugin-openh264 \
+mozilla-openh264
 
 ###
 # Install themes 
