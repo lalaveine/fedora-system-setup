815e2f6c0ba108e8ecfc44017fa7e6603a1460a8
Evgeniy Matveev <mfb.eugene@gmail.com>
2019-11-21 23:34:09 +0300

    Added script to symlink home folders to /data

--- setup.sh
+++ setup.sh
@@ -31,9 +31,28 @@ do
     shift
 done
 
+
+###
+# Symlink home folder to hard drive
+###
+
+home_folders=("Desktop" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos")
+
+# Remove folders from the home folder
+for folder_name in ${home_folders[@]}; do
+        rmdir $HOME/$folder_name
+done
+
+# Create system links from new location to the home folder
+for folder_name in ${home_folders[@]}; do
+        ln -s /data/$folder_name $HOME/$folder_name
+done
+
+
 ###
 # System configuration
 ###
+
 
 # Firefox
 bash -c 'cat > $HOME/.mozilla/firefox/$(ls $HOME/.mozilla/firefox/ | grep default-release)/user.js << EOL
