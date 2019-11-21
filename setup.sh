0fd788eb9a4fd7f7d3a286dc2b3a03c9a7bb9474
Evgen Matveev <mfb.eugene@gmail.com>
2019-11-21 22:34:06 +0000

    added folder creation in /data

--- setup.sh
+++ setup.sh
@@ -43,6 +43,11 @@ for folder_name in ${home_folders[@]}; do
         rmdir $HOME/$folder_name
 done
 
+# Create folders in the data folder
+for folder_name in ${home_folders[@]}; do
+        rmdir /data/$folder_name
+done
+
 # Create system links from new location to the home folder
 for folder_name in ${home_folders[@]}; do
         ln -s /data/$folder_name $HOME/$folder_name
