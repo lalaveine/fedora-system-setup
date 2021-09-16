47b5da38b5abbe0a649f6a0dedd2ce63d07c5f06
Evgenii Matveev <mfb.eugene@gmail.com>
2021-09-16 17:25:12 +0300

    Prepare for fedora 34 release

--- scripts/gnome-config.sh
+++ scripts/gnome-config.sh
@@ -5,12 +5,12 @@
 ###
 
 #Better Font Smoothing
-gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing 'rgba'
+#gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing 'rgba'
 
 #Usability Improvements
-gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'adaptive'
+#gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'adaptive'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+#gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
 
 #This indexer is nice, but can be detrimental for laptop users battery life
 gsettings set org.freedesktop.Tracker.Miner.Files index-on-battery false
@@ -18,9 +18,9 @@ gsettings set org.freedesktop.Tracker.Miner.Files index-on-battery-first-time fa
 gsettings set org.freedesktop.Tracker.Miner.Files throttle 15
 
 #Nautilus (File Manager) Usability
-gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'
-gsettings set org.gnome.nautilus.preferences executable-text-activation 'ask'
-gsettings set org.gnome.nautilus.list-view use-tree-view true
+#gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'
+#gsettings set org.gnome.nautilus.preferences executable-text-activation 'ask'
+#gsettings set org.gnome.nautilus.list-view use-tree-view true
 
 #Touchpad tap to click
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
@@ -29,7 +29,7 @@ gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.wm.preferences audible-bell false
 
 # Open gnome-terminal with Alt+Ctrl+T
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Open Terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Alt><Primary>T'
+#gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+#gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Open Terminal'
+#gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+#gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Alt><Primary>T'
